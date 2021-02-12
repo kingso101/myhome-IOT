ffabbacc5cce5fb1a065ce10f0fc7dbf4e1491b4
kings101 <okekekingsley101@gmail.com>
2021-02-12 05:24:19 +0100

    'Modified'

--- auth/login.php
+++ auth/login.php
@@ -10,7 +10,7 @@
         $password = htmlspecialchars(strip_tags($_POST['password']));
 
         if($_POST['action'] === 'login') {
-            $user = $db->$usersCollection->findOne(array('phone'=> $phone, 'password'=> $password));
+            $user = $db->$userCollection->findOne(array('phone'=> $phone, 'password'=> $password));
 
             if($user) {
                 print_r($user);
