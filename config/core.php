ffcc063bc8ebafb58a5f177938a941906404cd7f
kings101 <okekekingsley101@gmail.com>
2021-03-22 22:54:48 +0100

    'Modified'

---
+++ config/core.php
@@ -0,0 +1,14 @@
+<?php
+// start php session
+// session_start();
+// show error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ 
+// home page url
+$base_url="http://awsmyhome";
+ 
+// set your default time-zone
+date_default_timezone_set('Asia/Manila');
+
+?>
