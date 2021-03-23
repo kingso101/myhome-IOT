9d7a2faabcd2011a1ae658afe896290aa120a1e5
kings101 <okekekingsley101@gmail.com>
2021-03-23 01:56:23 +0100

    'Modified'

--- partials/header.inc.php
+++ partials/header.inc.php
@@ -1,12 +1,10 @@
 <?php session_start();
     // require_once 'functions.inc.php';
-    require_once(__DIR__ .'/../autoloaders.php');
-    // require_once(__DIR__ .'/../config/core.php');
-    // require_once(dirname(__FILE__) . "/../vendor/autoload.php");
-    // require_once(dirname(__FILE__) . "/../config/core.php");
-    // // $config = require_once(__DIR__ .'/../config/core.php');
-    // $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'./..');
-    // $dotenv->load();
+    require_once(__DIR__ .'/../vendor/autoload.php');
+    require_once(__DIR__ .'/../config/core.php');
+    // $config = require_once(__DIR__ .'/../config/core.php');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+    $dotenv->load();
 
     $s3_bucket = $_ENV['S3_KEY'];
 
