df4f072bf703cc6718831d04852bc5bb8d5af028
kings101 <okekekingsley101@gmail.com>
2021-03-24 11:23:03 +0100

    'Modified'

--- partials/header.inc.php
+++ partials/header.inc.php
@@ -3,7 +3,7 @@
     require_once(__DIR__ .'/../vendor/autoload.php');
     require_once(__DIR__ .'/../config/core.php');
     // $config = require_once(__DIR__ .'/../config/core.php');
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/..');
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'..');
     $dotenv->load();
 
     // try {
